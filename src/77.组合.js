19936b228f7c419880a4af3be5d2104c1fc4c6d5
liang.cheng <glimmer.treasure@gmail.com>
2024-11-01 19:47:23 +0800

    feat: 完成“77.组合”

---
+++ src/77.组合.js
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode.cn id=77 lang=javascript
+ *
+ * [77] 组合
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number[][]}
+ */
+var combine = function(n, k) {
+    let result = []
+    let list = []
+    let permutation = []
+    function backtracking(deep, start) {
+        if (deep < 1) {
+            result.push([...permutation])
+            return
+        } else {
+            for(let i = start; i < list.length; i++) {
+                let ele = list[i]
+                permutation.push(ele)
+                backtracking(deep - 1, i + 1)
+                permutation.pop()
+            }
+        }
+    }
+    for (let i = 1; i <= n; i++) {
+        list.push(i)
+    }
+    backtracking(k, 0, [])
+    return result
+};
+
+// @lc code=end
+
